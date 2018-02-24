77b27ccaad5546e6fc612276ad2ae0f89aff6192
QUAN THE TRAN <qtran88@cs.washington.edu>
2018-02-24 13:41:18 -0800

    add sortedTest

--- SortedTest.java
+++ SortedTest.java
@@ -1,5 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SortedTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void insertionSorted() {
+        int[] array = {5,7,1,2,4,6,0};
+        int[] arraySorted = {0,1,2,4,5,6,7};
+        Sorted theSorted = new Sorted();
+        theSorted.insertionSorted(array);
+        for (int i = 0; i < array.length; i++) {
+            System.err.println(array[i]);
+            System.err.println(arraySorted[i]);
+            assertTrue(array[i] == arraySorted[i]);
+        }
+    }
 
 }
